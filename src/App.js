4bce175cf90d3e7e28cbba554ac51418dd1585b1
dalipkumar703 <dalipkumar703@gmail.com>
2020-09-14 19:08:54 +0530

    Add fractaltask

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef} from 'react';
+import {FormControl, Collapse, InputGroup, Button, Row, Container} from 'react-bootstrap'
+import {useDispatch} from 'react-redux';
 
-function App() {
+import './App.css';
+import {BucketList} from './BucketComponent';
+import {addTodo} from './actions';
+
+const App = () => {
+  const dispatch = useDispatch();
+  //bucket name
+  const [bucketName, setBucketName] = useState('');
+  //show buck popup
+  const [open, setOpen] = useState(false);
+  //input ref
+  const pageRef = useRef(null);
+  // const prevBucket = usePrevious(buckets);
+  
+  useEffect(() => {
+    if (pageRef.current.className.indexOf('inputSearch') < 0){
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  },[pageRef])
+
+  const handleOnChange = (event) => { 
+      setBucketName(event.target.value);
+  }
+
+  const handleOnClick = () => {
+    if (bucketName.length > 0){
+      dispatch(addTodo({name: bucketName}));
+      setBucketName('');
+    }
+  }
+  
   return (
-    <div className="App">
+    
+    <div className="App" ref={pageRef}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container>
+        <Row>
+      <InputGroup className="mb-3">
+    <FormControl
+      placeholder="Add Todo"
+      className="inputSearch"
+      aria-label="Recipient's username"
+      aria-describedby="basic-addon2"
+      aria-expanded={open}
+    onChange={handleOnChange}
+    onFocusOut={()=>setOpen(false)}
+    value={bucketName}
+    />
+    <InputGroup.Append>
+      <Button variant="primary" onClick={handleOnClick}>Add</Button>
+    </InputGroup.Append>
+    <br></br>
+    <Collapse in={open}>
+      <div>
+       <BucketList></BucketList>
+       </div>
+      </Collapse>
+  </InputGroup>
+  <BucketList></BucketList>
+    </Row>
+  </Container>
       </header>
     </div>
   );
